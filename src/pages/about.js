7a1117f1ddc541be8b7df29ed2ec730f64a4a9e3
Phil <nguy.philip@gmail.com>
2020-09-11 15:40:28 -0400

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+    return (
+        <div style={{ color: `coral` }}>
+            <Header headerText="About Gatsby"/>
+            <Header headerText="Another reusable comp"/>
+            <p>Such wow. Very Components.</p>
+        </div>
+    );
+}
